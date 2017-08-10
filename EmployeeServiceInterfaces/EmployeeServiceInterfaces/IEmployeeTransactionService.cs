4fb8641f7735220e6547c196f168dcd3aedec6df
Emmanuel Rosales <emmanuelrs@gmail.com>
2017-08-10 08:25:04 -0600

    Intercace Class

---
+++ EmployeeServiceInterfaces/EmployeeServiceInterfaces/IEmployeeTransactionService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using System.Runtime.Serialization;
+
+namespace EmployeeServiceInterfaces
+{
+    [ServiceContract]
+    public interface IEmployeeTransactionService
+    {
+        [OperationContract]
+        void employeeTransaction(Employee employee);
+
+        [OperationContract]
+        Employee[] GetEmployeesTransaction();
+    }
+
+    [DataContract]
+    public class Employee
+    {
+        private string employee_name;
+
+        [DataMember]
+        public string EmployeeName
+        {
+            set { this.employee_name = value; }
+            get { return this.employee_name; }
+        }
+    }
+}
